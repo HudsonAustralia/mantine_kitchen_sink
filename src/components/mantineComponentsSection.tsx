2419e39496d019a4914459ea7fb4e918276850ff
Hudson <hudson.sydney.software@gmail.com>
2023-06-05 09:48:45 +1000

    update

--- src/components/mantineComponentsSection.tsx
+++ src/components/mantineComponentsSection.tsx
@@ -1,14 +1,22 @@
 import React from "react";
-import { Button, Flex, ActionIcon } from "@mantine/core";
+import { Button, Flex, ActionIcon, Group, CloseButton } from "@mantine/core";
 import { Adjustments } from "tabler-icons-react";
+import { Settings } from "tabler-icons-react";
 
 const MantineComponents = () => {
   return (
-    <Flex>
+    <Flex justify={"center"} mt={12}>
       <Button>Settings</Button>
       <ActionIcon>
         <Adjustments size="1.125rem" />
       </ActionIcon>
+      <ActionIcon variant="transparent">
+        <Settings size="1rem" />
+      </ActionIcon>
+      <Group position="center">
+        <CloseButton aria-label="Close modal" />
+        <CloseButton title="Close popover" size="xl" iconSize={20} />
+      </Group>
     </Flex>
   );
 };
--- src/themes/customLightTheme.tsx
+++ src/themes/customLightTheme.tsx
@@ -3,20 +3,21 @@ import { MantineThemeOverride } from "@mantine/core";
 const theme: MantineThemeOverride = {
   colors: {
     // override dark colors to change them for all component
-    brand: [
-      "#F0BBDD",
-      "#ED9BCF",
-      "#EC7CC3",
-      "#ED5DB8",
-      "#F13EAF",
-      "#F71FA7",
-      "#FF00A1",
-      "#E00890",
-      "#C50E82",
-      "#AD1374",
-    ],
+    // brand: [
+    //   "#F0BBDD",
+    //   "#ED9BCF",
+    //   "#EC7CC3",
+    //   "#ED5DB8",
+    //   "#F13EAF",
+    //   "#F71FA7",
+    //   "#FF00A1",
+    //   "#E00890",
+    //   "#C50E82",
+    //   "#AD1374",
+    // ],
   },
-  primaryColor: "brand",
+  fontSizes: {},
+  // primaryColor: "brand",
 };
 
 export default theme;
